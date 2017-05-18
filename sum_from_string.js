7753abfd63227b69e4d840d13ffd6b394b8844de
Nawar <nawaryossef2@gmail.com>
2017-05-17 23:06:31 -0400

    sum_from_string solution

---
+++ sum_from_string.js
@@ -0,0 +1,11 @@
+// challenge : 
+// Problem. Given two digits a and b, find a+b.
+// Input format. The first line of the input contains two integers a and b (separated by a space).
+// Constraints. 0≤a,b≤9.
+// Output format. Output a+b.
+
+// Solution: 
+var input = "3 2"
+var firstNum = Number(input.split(" ")[0])
+var SecondNum = Number(input.split(" ")[1])
+console.log(firstNum + SecondNum)
